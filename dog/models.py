5a07bac1badb42213aca8a506e931d2645a04702
jarci55 <33427618+jarci55@users.noreply.github.com>
2018-03-10 19:06:58 +0000

    models updated

--- dog/models.py
+++ dog/models.py
@@ -17,12 +17,24 @@ class Region(models.Model):
 
 class Cottage(models.Model):
     region = models.ForeignKey(Region)
-    title = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='cottages', blank=True)
     address = models.CharField(max_length=128, blank=True)
     views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Cottage, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+'''    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+'''
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
