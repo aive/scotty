c9e15ce2d750628767473ac920d176e8cff4e138
jarci55 <33427618+jarci55@users.noreply.github.com>
2018-03-10 18:59:39 +0000

    Updated population file

--- populate_script.py
+++ populate_script.py
@@ -12,41 +12,42 @@ from dog.models import Cottage
 def populate():
 
     highlands_cottages = [
-    {"title": "Aberdeenshire", "address":"AB1","views": 20},
-    {"title": "Applecross", "address":"IV54","views": 21},
-    {"title": "Stirling", "address":"FK15","views": 10},
-    {"title": "Alvemore","address":"PH22","views": 220},
-    {"title": "BettyHill", "address":"KW14","views": 10},
-    {"title": "Braemar", "address":"AB35","views": 520},
-    {"title": "Cannich", "address":"IV4","views": 520},
-    {"title": "Corpach","address":"PH33","views": 820},
-    {"title": "Dalwhinnie", "address":"PH19","views": 22},
-    {"title": "Edderton","address":"IV19", "views": 20}
+    {"name": "Aberdeenshire", "image": "Aberdeenshire.jpg", "address":"AB1","views": 20},
+    {"name": "Applecross", "image": "Applecross.jpg", "address":"IV54","views": 21},
+    {"name": "Stirling", "image": "Stirling.jpg", "address":"FK15","views": 10},
+    {"name": "Alvemore", "image": "Alvemore.jpg","address":"PH22","views": 220},
+    {"name": "BettyHill", "image": "BettyHill.jpg", "address":"KW14","views": 10},
+    {"name": "Braemar", "image": "Braemar.jpg", "address":"AB35","views": 520},
+    {"name": "Cannich", "image": "Cannich.jpg", "address":"IV4","views": 520},
+    {"name": "Corpach", "image": "Corpach.jpg","address":"PH33","views": 820},
+    {"name": "Dalwhinnie", "image": "Dalwhinnie.jpg", "address":"PH19","views": 22},
+    {"name": "Edderton", "image": "Edderton.jpg","address":"IV19", "views": 20}
 
     ]
 
 
     lowlands_cottages = [
-    {"title": "Guildford", "address":"GU4","views": 20},
-    {"title": "Hudderfield", "address":"HD2","views": 21},
-    {"title": "Dunbane", "address":"FK15","views": 10},
-    {"title": "Hereford", "address":"HR2","views": 10},
-    {"title": "Beauly", "address":"IV4","views": 520},
-    {"title": "Ayr", "address":"KA7","views": 420},
-    {"title": "Cupar", "address":"KY15", "views": 20}
+    {"name": "Guildford", "image": "Guildford.jpg", "address":"GU4","views": 20},
+    {"name": "Hudderfield", "image": "Hudderfield.jpg", "address":"HD2","views": 21},
+    {"name": "Dunbane", "image": "Dunbane.jpg", "address":"FK15","views": 10},
+    {"name": "Hereford", "image": "Hereford.jpg", "address":"HR2","views": 10},
+    {"name": "Beauly", "image": "Beauly.jpg", "address":"IV4","views": 520},
+    {"name": "Ayr", "image": "Ayr.jpg", "address":"KA7","views": 420},
+    {"name": "Cupar", "image": "Cupar.jpg", "address":"KY15", "views": 20}
     ]
 
 
     islands_cottages = [
-    {"title": "Belfast","address":"BT1","views": 20},
-    {"title": "Newcastle","address":"BT33","views": 21},
-    {"title": "Ballymena","address":"BT44","views": 10},
-    {"title": "Newtownnards","address":"BT52","views": 230},
-    {"title": "Dungannon", "address":"BT70","views": 12},
-    {"title": "Isle of Man","address":" IM3","views": 520},
-    {"title": "Isles of Scilly","address":"TR23","views": 230}
+    {"name": "Belfast", "image": "Belfast.jpg","address":"BT1","views": 20},
+    {"name": "Newcastle", "image": "Newcastle.jpg","address":"BT33","views": 21},
+    {"name": "Ballymena", "image": "Ballymena.jpg","address":"BT44","views": 10},
+    {"name": "Newtownnards", "image": "Newtownnards.jpg","address":"BT52","views": 230},
+    {"name": "Dungannon", "image": "Dungannon.jpg", "address":"BT70","views": 12},
+    {"name": "Isle of Man", "image": "Isle of Man.jpg","address":" IM3","views": 520},
+    {"name": "Isles of Scilly", "image": "Isles of Scilly.jpg","address":"TR23","views": 230}
 
     ]
+    
 
 
     regs = {"Highlands": {"cottages": highlands_cottages, "views": 128, "likes": 64},
@@ -56,15 +57,15 @@ def populate():
     for reg, reg_data in regs.items():
         r = add_reg(reg, reg_data["views"], reg_data["likes"])
         for c in reg_data["cottages"]:
-            add_cottage(r, c["title"], c["address"], c["views"])
-
+            add_cottage(r, c["name"], c["address"], c["views"])
+        
     for r in Region.objects.all():
         for c in Cottage.objects.filter(region=r):
             print("- {0} - {1}".format(str(r), str(c)))
 
 
-def add_cottage(reg, title, address, views=0):
-        c = Cottage.objects.get_or_create(region=reg, title=title)[0]
+def add_cottage(reg, name, address, views=0):
+        c = Cottage.objects.get_or_create(region=reg, name=name)[0]
         c.address=address
         c.save()
         return c
@@ -76,5 +77,6 @@ def add_reg(name, views=0, likes=0):
         return r
 
 if __name__ == '__main__':
-        print("Starting BnB population script...")
+        print("Starting population script...")
         populate()
+
