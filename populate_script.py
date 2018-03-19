d263e3d3998b56ce819e89a68aa7b426dd43cd22
Aive Kalmus <2288799k@student.gla.ac.uk>
2018-03-19 18:38:21 +0000

    added descriptions to populate_script.py

--- populate_script.py
+++ populate_script.py
@@ -12,39 +12,39 @@ from dog.models import Cottage
 def populate():
 
     highlands_cottages = [
-    {"name": "The Burrow", "area":"Aberdeenshire", "image": "cottages/The Burrow.jpg", "address":"AB51 4GY","views": 20},
-    {"name": "Windyridge", "area": "Applecross", "image": "cottages/Windyridge.jpg", "address":"IV54 8LR","views": 21},
-    {"name": "Garden Cottage", "area": "Stirling", "image": "cottages/Garden Cottage.jpg", "address":"FK15 0HG","views": 10},
-    {"name": "Four Winds", "area": "Aviemore", "image": "cottages/Four Winds.jpg","address":"PH22 1RB","views": 220},
-    {"name": "Wild Bank", "area": "BettyHill", "image": "cottages/Wild Bank.jpg", "address":"KW14 8TY","views": 10},
-    {"name": "Mile End", "area": "Braemar", "image": "cottages/Mile End.jpg", "address":"AB35 5XU","views": 520},
-    {"name": "Dreamwood", "area": "Cannich", "image": "cottages/Dreamwood.jpg", "address":"IV4 7HR","views": 520},
-    {"name": "Stillness", "area": "Corpach", "image": "cottages/Stillness.jpg","address":"PH33 6PF","views": 820},
-    {"name": "Bridgelands", "area": "Dalwhinnie", "image": "cottages/Bridgelands.jpg", "address":"PH19 1AA","views": 22},
-    {"name": "Dengarden", "area": "Edderton", "image": "cottages/Dengarden.jpg","address":"IV19 1EH", "views": 20}
+    {"name": "The Burrow","description": "Cozy place in Aberdeenshire.", "area":"Aberdeenshire", "image": "cottages/The Burrow.jpg", "address":"AB51 4GY","views": 20},
+    {"name": "Windyridge", "description": "Nice place in Applecross.", "area": "Applecross", "image": "cottages/Windyridge.jpg", "address":"IV54 8LR","views": 21},
+    {"name": "Garden Cottage", "description": "Big garden full of fun!", " "area": "Stirling", "image": "cottages/Garden Cottage.jpg", "address":"FK15 0HG","views": 10},
+    {"name": "Four Winds", "description": "Be free as the wind!", "area": "Aviemore", "image": "cottages/Four Winds.jpg","address":"PH22 1RB","views": 220},
+    {"name": "Wild Bank", "description": "Go wild, river and rafting!",  "area": "BettyHill", "image": "cottages/Wild Bank.jpg", "address":"KW14 8TY","views": 10},
+    {"name": "Mile End", "description": "The beginning of something new!", "area": "Braemar", "image": "cottages/Mile End.jpg", "address":"AB35 5XU","views": 520},
+    {"name": "Dreamwood", "description": "Woods for chasing squirrels", "area": "Cannich", "image": "cottages/Dreamwood.jpg", "address":"IV4 7HR","views": 520},
+    {"name": "Stillness", "description": "Beautiful remote cottage", "area": "Corpach", "image": "cottages/Stillness.jpg","address":"PH33 6PF","views": 820},
+    {"name": "Bridgelands", "description": "Best holiday place for families and dogs. ", "area": "Dalwhinnie", "image": "cottages/Bridgelands.jpg", "address":"PH19 1AA","views": 22},
+    {"name": "Dengarden", "description": "Underground adventures. ", "area": "Edderton", "image": "cottages/Dengarden.jpg","address":"IV19 1EH", "views": 20}
 
     ]
 
 
 
     lowlands_cottages = [
-    {"name": "Kuredu", "area":"Guildford", "image": "cottages/Kuredu.jpg", "address":"GU4 8SE","views": 20},
-    {"name": "Cumfrubrum", "area":"Hudderfield", "image": "cottages/Cumfrubrum.jpg", "address":"HD2 1YY","views": 21},
-    {"name": "Chestnuts", "area":"Dunbane", "image": "cottages/Chestnuts.jpg", "address":"FK15 0NB","views": 10},
-    {"name": "The Warren", "area":"Hereford", "image": "cottages/The Warren.jpg", "address":"HR2 6JT","views": 10},
-    {"name": "The Cuckoo's Nest", "area":"Beauly", "image": "cottages/The Cuckoo's Nest.jpg", "address":"IV40 8DX","views": 520},
-    {"name": "Crystal Cottage", "area":"Ayr", "image": "cottages/Crystal Cottage.jpg", "address":"KA7 4PQ","views": 420},
-    {"name": "Kites Farm", "area":"Cupar", "image": "cottages/Kites Farm.jpg", "address":"KY15 7HY", "views": 20}
+    {"name": "Kuredu", "description": "We have lots of space. ", "area":"Guildford", "image": "cottages/Kuredu.jpg", "address":"GU4 8SE","views": 20},
+    {"name": "Cumfrubrum", "description": "Lots of adventures for dogs. ", "area":"Hudderfield", "image": "cottages/Cumfrubrum.jpg", "address":"HD2 1YY","views": 21},
+    {"name": "Chestnuts", "description": "Firehyrdant in yard and cozy dogbed ", "area":"Dunbane", "image": "cottages/Chestnuts.jpg", "address":"FK15 0NB","views": 10},
+    {"name": "The Warren", "description": "Everything your dog has ever dreamed of", "area":"Hereford", "image": "cottages/The Warren.jpg", "address":"HR2 6JT","views": 10},
+    {"name": "The Cuckoo's Nest", "description": "Go crazy! Or just have a god rest. ", "area":"Beauly", "image": "cottages/The Cuckoo's Nest.jpg", "address":"IV40 8DX","views": 520},
+    {"name": "Crystal Cottage", "description": "Discover Magnificent Ayr in its glory.", "area":"Ayr", "image": "cottages/Crystal Cottage.jpg", "address":"KA7 4PQ","views": 420},
+    {"name": "Kites Farm", "description": "Kites, wind, surf in costal Cupar ", "area":"Cupar", "image": "cottages/Kites Farm.jpg", "address":"KY15 7HY", "views": 20}
     ]
 
     islands_cottages = [
-    {"name": "Robins Hedge", "area":"Belfast", "image": "cottages/Robins Hedge.jpg","address":"BT18 9JQ","views": 20},
-    {"name": "Heatherbell", "area":"Newcastle", "image": "cottages/Heatherbell.jpg","address":"BT3 9JL","views": 21},
-    {"name": "Chimney Cottage", "area":"Ballymena", "image": "cottages/Chimney Cottage.jpg","address":"BT44 8SB","views": 10},
-    {"name": "Rosemary House", "area":"Newtownnards", "image": "cottages/Rosemary House.jpg","address":"BT52 2NS","views": 230},
-    {"name": "Mulberry", "area":"Dungannon", "image": "cottages/Mulberry.jpg", "address":"BT70 2HW","views": 12},
-    {"name": "Stonehurst", "area":"Isle of Man", "image": "cottages/Stonehurst.jpg","address":"IM3 1BB","views": 520},
-    {"name": "Seacrest", "area":"Isles of Scilly", "image": "cottages/Seacrest.jpg","address":"TR23 0WA","views": 230}
+    {"name": "Robins Hedge", "description": "Good for birdwatchers. ", "area":"Belfast", "image": "cottages/Robins Hedge.jpg","address":"BT18 9JQ","views": 20},
+    {"name": "Heatherbell", "description": "Hunting lodge in the heathers. ", "area":"Newcastle", "image": "cottages/Heatherbell.jpg","address":"BT3 9JL","views": 21},
+    {"name": "Chimney Cottage", "description": "Warm cottage in Ballymena. ", "area":"Ballymena", "image": "cottages/Chimney Cottage.jpg","address":"BT44 8SB","views": 10},
+    {"name": "Rosemary House", "description": "Dogs welcome! Delecious roast dinner and warm bed. ", "area":"Newtownnards", "image": "cottages/Rosemary House.jpg","address":"BT52 2NS","views": 230},
+    {"name": "Mulberry", "description": "Exciting country walks nearby ", "area":"Dungannon", "image": "cottages/Mulberry.jpg", "address":"BT70 2HW","views": 12},
+    {"name": "Stonehurst", "description": "Just come along and see. ", "area":"Isle of Man", "image": "cottages/Stonehurst.jpg","address":"IM3 1BB","views": 520},
+    {"name": "Seacrest", "description": "Holiday home on Isles of Scilly ", "area":"Isles of Scilly", "image": "cottages/Seacrest.jpg","address":"TR23 0WA","views": 230}
 
     ]
 
