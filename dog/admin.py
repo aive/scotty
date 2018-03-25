f9ee31ad0a3856e4d4a170b3b13bf9dec1deb484
jarci55 <33427618+jarci55@users.noreply.github.com>
2018-03-25 13:07:27 +0100

    Added comments

--- dog/admin.py
+++ dog/admin.py
@@ -4,13 +4,12 @@ from dog.models import UserProfile
 
 
 class CottageAdmin(admin.ModelAdmin):
-    prepoulated_fields = {'slug':('title',)}
-
+    prepopulated_fields = {'slug':('name',)}
 class RegionAdmin(admin.ModelAdmin):
-    prepoulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug':('name',)}
 
 class UserProfileAdmin(admin.ModelAdmin):
-    prepoulated_fields = {'slug':('user',)}
+    prepopulated_fields = {'slug':('user',)}
 
 #from dog.models import Cottage
 admin.site.register(UserProfile)
--- dog/forms.py
+++ dog/forms.py
@@ -32,15 +32,6 @@ class CottageForm(forms.ModelForm):
         fields = ('name', 'image', 'address')
         exclude = ('region','user', 'likes', 'views')
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-
-        if url and not url.startswith('http://'):
-            url = 'http://' + url
-            cleaned_data['url'] = url
-
-            return cleaned_data
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
